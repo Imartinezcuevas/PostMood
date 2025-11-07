409dea9d2a13642c9f4fb6d0d545163914893ddf
Iván Martínez Cuevas <imartinezcuevas@gmail.com>
2025-11-07 08:52:02 +0100

    Moved redis to API Gateway

--- services/api-gateway/app.py
+++ services/api-gateway/app.py
@@ -6,6 +6,9 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from asyncio import sleep
+from redis import asyncio as aioredis
+import json
+import time
 
 # ----------------------
 # Configuration
@@ -17,12 +20,38 @@ SENTIMENT_ANALYZER_URL = os.getenv("SENTIMENT_ANALYZER_URL", "http://sentiment-a
 
 app = FastAPI(title="API Gateway")
 
+# ------------------
+# Redis setup
+# -----------------
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+REDIS_TTL = int(os.getenv("REDIS_TTL", 600))
+redis = None
+
+# ------------------
+# Events
+# -----------------
+@app.on_event("startup")
+async def startup():
+    global redis
+    try:
+        redis = await aioredis.from_url(f"redis://{REDIS_HOST}:{REDIS_PORT}", encoding="utf-8", decode_responses=True)
+        logging.info(f"Connected to Redis at {REDIS_HOST}:{REDIS_PORT}")
+    except Exception as e:
+        logging.error(f"Failed to connect to Redis: {e}")
+        raise
+
+@app.on_event("shutdown")
+async def shutdown():
+    if redis:
+        await redis.close()
+        logging.info("Redis connection closed.")
+
 # ----------------------
 # Models
 # ----------------------
 class SearchRequest(BaseModel):
     keyword: str
-    limit: int = 25
 
 # ----------------------
 # Endpoints
@@ -39,26 +68,54 @@ def health_check():
 @app.post("/search")
 async def search_posts(request: SearchRequest):
     search_id = str(uuid.uuid4())
-    logging.info(f"[{search_id}] New search: {request.keyword} (limit={request.limit})")
+    logging.info(f"[{search_id}] New search: {request.keyword}")
+
+    cache_key = f"reddit:{request.keyword}"
+    if redis:
+        cached_data = await redis.get(cache_key)
+        if cached_data:
+            logging.info(f"Cache hit for '{request.keyword}'")
+            cached = json.loads(cached_data)
+            return {
+                "search_id": search_id,
+                "keyword": request.keyword,
+                **cached
+            }
 
     async with httpx.AsyncClient(timeout=httpx.Timeout(20.0)) as client:
-        # 1️⃣ Llamada al reddit-worker
-        reddit_resp = await client.post(f"{REDDIT_WORKER_URL}/search", json=request.dict())
-        reddit_resp.raise_for_status()
-        posts = reddit_resp.json()["posts"]
+        # 1️. Llamada al reddit-worker
+        try:
+            reddit_resp = await client.post(f"{REDDIT_WORKER_URL}/search", json=request.dict())
+            reddit_resp.raise_for_status()
+            posts = reddit_resp.json()["posts"]
+        except Exception as e:
+            logging.error(f"[{search_id}] Reddit worker failed: {e}")
+            raise HTTPException(status_code=502, detail="Reddit worker unavailable")
 
-        # 2️⃣ Llamada al sentiment-analyzer
-        texts = [f"{p['title']} {p['text']}" for p in posts]
-        sentiment_resp = await client.post(f"{SENTIMENT_ANALYZER_URL}/analyze", json={"posts": texts})
-        sentiment_resp.raise_for_status()
-        sentiments = sentiment_resp.json()["results"]
+        # 2️. Llamada al sentiment-analyzer
+        try: 
+            texts = [f"{p['title']} {p['text']}" for p in posts]
+            sentiment_resp = await client.post(f"{SENTIMENT_ANALYZER_URL}/analyze", json={"posts": texts})
+            sentiment_resp.raise_for_status()
+            sentiments = sentiment_resp.json()["results"]
+        except Exception as e:
+            logging.error(f"[{search_id}] Sentiment analyzer failed: {e}")
+            raise HTTPException(status_code=502, detail="Sentiment analyzer unavailble")
 
-        # 3️⃣ Agregar sentimiento al resultado
-        for post, sent in zip(posts, sentiments):
-            post.update(sent)
+    # 3️ Agregar sentimiento al resultado
+    for post, sent in zip(posts, sentiments):
+        post.update(sent)
 
-        return {
-            "search_id": search_id,
-            "keyword": request.keyword,
-            "results": posts,
+    if redis and posts:
+        payload = {
+            "results": post,
+            "cached_at": time.time()
         }
+        await redis.set(cache_key, json.dumps(payload), ex=REDIS_TTL)
+        logging.info(f"Stored '{request.keyword}' results in cache ({REDIS_TTL}s TTL).")
+
+    return {
+        "search_id": search_id,
+        "keyword": request.keyword,
+        "results": posts,
+    }
--- services/reddit-worker/ingestor.py
+++ services/reddit-worker/ingestor.py
@@ -8,7 +8,6 @@ import asyncpraw
 from dotenv import load_dotenv
 from pathlib import Path
 import logging
-from redis import asyncio as aioredis
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
@@ -18,16 +17,9 @@ from psycopg2.extras import RealDictCursor
 logging.basicConfig(level=logging.INFO)
 env_path = Path(__file__).resolve().parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
+LIMIT = int(os.getenv("LIMIT", 50))
 
 app = FastAPI(title="Reddit Worker")
-
-# ------------------
-# Redis setup
-# -----------------
-REDIS_HOST = os.getenv("REDIS_HOST", "redis")
-REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
-REDIS_TTL = int(os.getenv("REDIS_TTL", 600))
-redis = None
 
 # ------------------
 # Reddit setup
@@ -67,7 +59,6 @@ def insert_post(conn, post):
 # -----------------
 class KeywordRequest(BaseModel):
     keyword: str
-    limit: int = 20
 
 # -------------------
 # Cleaning
@@ -82,25 +73,6 @@ def clean_text(text: str):
     return text.lower()
 
 # ------------------
-# Events
-# -----------------
-@app.on_event("startup")
-async def startup():
-    global redis
-    try:
-        redis = await aioredis.from_url(f"redis://{REDIS_HOST}:{REDIS_PORT}", encoding="utf-8", decode_responses=True)
-        logging.info(f"Connected to Redis at {REDIS_HOST}:{REDIS_PORT}")
-    except Exception as e:
-        logging.error(f"Failed to connect to Redis: {e}")
-        raise
-
-@app.on_event("shutdown")
-async def shutdown():
-    if redis:
-        await redis.close()
-        logging.info("Redis connection closed.")
-
-# ------------------
 # Routs
 # -----------------
 @app.get("/health")
@@ -110,23 +82,11 @@ def health_check():
 @app.post("/search")
 async def search_posts(request: KeywordRequest):
     logging.info(f"Received request: {request}")
-
-    # 1. Search in cache
-    cache_key = f"reddit:{request.keyword}:{request.limit}"
-    if redis:
-        cached_data = await redis.get(cache_key)
-        if cached_data:
-            logging.info(f"Cache hit for '{request.keyword}'")
-            return {
-                "from_cache": True,
-                "posts": json.loads(cached_data)
-            }
-    # 2. Search in reddit
     try:
         posts = []
         subreddit = await reddit.subreddit("all")
 
-        async for submission in subreddit.search(request.keyword, limit=request.limit):
+        async for submission in subreddit.search(request.keyword, limit=LIMIT):
             post = {
                 "id": submission.id,
                 "title": clean_text(submission.title),
@@ -142,12 +102,7 @@ async def search_posts(request: KeywordRequest):
                 "title": post["title"],
                 "text": post["selftext"]
             })
-    
-    # 3. Save in cache
-        if redis and posts:
-            await redis.set(cache_key, json.dumps(posts), ex=REDIS_TTL)
-            logging.info(f"Stored '{request.keyword}' results in cache ({REDIS_TTL}s TTL).")
-        
+
         return {
             "from_cache": False,
             "posts": posts
